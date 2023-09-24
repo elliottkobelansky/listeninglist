8bc86202b964cc8f07eb1e1dd43d13d6527a9eec
Elliott Kobelansky <elliottkobelansky@Elliotts-Air.lan>
2023-09-24 08:22:26 -0400

    should finally fix

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import pieces from './pieces.json' assert { type: 'json' };
+const { default: pieces } = await import("./pieces.json", { assert: { type: "json" } })
 
 var currentAudio = "";
 var currentName = "";
